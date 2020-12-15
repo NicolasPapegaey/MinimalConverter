d1cc3596779f964b28bed6787b83724d5f3cd340
Nicolas Papegaey <nicolas.papegaey@gmail.com>
2020-12-15 07:55:28 +0100

    Initial Commit

---
+++ MinimalConverter/MinimalConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MinimalConverterApp.swift
+//  MinimalConverter
+//
+//  Created by Nicolas Papegaey on 15/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct MinimalConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
